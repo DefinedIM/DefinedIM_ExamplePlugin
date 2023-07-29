2a14ed742da968f61939d8c9af89a7fe54aa7039
Tony Shang <64829162+TonyNomoney@users.noreply.github.com>
2023-07-29 14:02:58 +0800

    继续修

--- src/main/java/org/definedim/example/ExamplePlugin.java
+++ src/main/java/org/definedim/example/ExamplePlugin.java
@@ -2,15 +2,31 @@ package org.definedim.example;
 
 import com.alibaba.fastjson2.JSON;
 import org.definedim.DefinedIM;
+import org.definedim.DefinedIMServer;
 import org.definedim.plugin.DefinedIMPlugin;
 import org.definedim.plugin.PluginManager;
 
+import java.lang.reflect.Field;
+import java.net.URLClassLoader;
+
 public class ExamplePlugin extends DefinedIMPlugin {
     @Override
-    public void onRegister() {
+    public void onLoad() {
         System.out.println("Test Plugin Loaded!");
-        System.out.println(JSON.toJSONString(config));
-        System.out.println(JSON.toJSONString(DefinedIM.pluginManager));
+//        URLClassLoader urlClassLoader = (URLClassLoader) Thread.currentThread().getContextClassLoader();
+//        ClassLoader classLoader = urlClassLoader.getParent();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        try {
+            Class<?> clazz = classLoader.loadClass("org.definedim.DefinedIMServer");
+            Field clazzField = clazz.getDeclaredField("definedIMServer");
+            clazzField.setAccessible(true);
+            Object obj = clazzField.get(null);
+            DefinedIMServer server = (DefinedIMServer) obj;
+            System.out.println(JSON.toJSONString(server));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
